cdddd746afcebf398bf34e290bb7b86c1c8b8001
flysnob <flysnob@gmail.com>
2018-06-05 04:26:29 -0600

    fixed tabs, added has_contents? to question

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,8 @@ class Question < ActiveRecord::Base
   belongs_to :subject
   belongs_to :version
   has_many :version_nodes
+
+  def has_contents?
+    summary.present? || help.present? || examples.present? || faq.present? || asc.present?
+  end
 end
